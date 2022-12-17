176b771ebdd1bdf5090a400708ba01b9dc7da137
meshtag <prathameshtagore@gmail.com>
2022-12-17 22:13:49 +0530

    First improvement iteration

--- tmp.mlir
+++ tmp.mlir
@@ -2,7 +2,10 @@
   "func.func"() ({
     %0 = "arith.constant"() {"value" = 0 : index} : () -> index
     %1 = "memref.alloca"() {"alignment" = 0 : i64, "operand_segment_sizes" = dense<[0, 0]> : vector<2xi32>} : () -> memref<1xindex>
-    %2 = "vector.load"(%1, %0) : (memref<1xindex>, index) -> index
+    %2 = "vector.load"(%1, %0) : (memref<1xindex>, index) -> vector<1xindex>
+    "vector.store"(%2, %1, %0) : (vector<1xindex>, memref<1xindex>, index) -> ()
+    %3 = "vector.fma"(%2, %2, %2) : (vector<1xindex>, vector<1xindex>, vector<1xindex>) -> vector<1xindex>
+    "memref.dealloc"(%1) : (memref<1xindex>) -> ()
     "func.return"() : () -> ()
   }) {"sym_name" = "vector_test", "function_type" = () -> (), "sym_visibility" = "private"} : () -> ()
 }) : () -> ()
--- xdsl/dialects/vector.py
+++ xdsl/dialects/vector.py
@@ -20,7 +20,7 @@ class Load(Operation):
     name = "vector.load"
     memref: Annotated[SSAValue, OperandDef(MemRefType)]
     indices: Annotated[list[SSAValue], VarOperandDef(IndexType)]
-    res: Annotated[OpResult, ResultDef(AnyAttr())]
+    res: Annotated[OpResult, ResultDef(VectorType)]
 
     # TODO varargs for indexing, which must match the vector dimensions
     # Problem: vector dimensions require variadic type parameters,
@@ -29,9 +29,9 @@ class Load(Operation):
     # Add variable vector length parameter feature
 
     def verify_(self):
-        if self.memref.typ.element_type != self.res.typ:
-            raise Exception(
-                "expected return type to match the Vector element type")
+        # if self.memref.typ.element_type != self.res.typ:
+        #     raise Exception(
+        #         "expected return type to match the Vector element type")
 
         if self.memref.typ.get_num_dims() != len(self.indices):
             raise Exception("expected an index for each dimension")
@@ -42,6 +42,47 @@ class Load(Operation):
         return Load.build(operands=[ref, indices],
                           result_types=[SSAValue.get(ref).typ.element_type])
 
+@irdl_op_definition
+class Store(Operation):
+    name = "vector.store"
+    vector: Annotated[SSAValue, OperandDef(VectorType)]
+    memref: Annotated[SSAValue, OperandDef(MemRefType)]
+    indices: Annotated[list[SSAValue], VarOperandDef(IndexType)]
 
+    # Add variable vector length parameter feature
 
-Vector = Dialect([Load], [])
+    def verify_(self):
+        # if self.memref.typ.element_type != self.value.typ:
+        #     raise Exception(
+        #         "Expected value type to match the MemRef element type")
+
+        if self.memref.typ.get_num_dims() != len(self.indices):
+            raise Exception("Expected an index for each dimension")
+
+    @staticmethod
+    def get(vector: Operation | SSAValue, ref: Operation | SSAValue,
+            indices: List[Operation | SSAValue]) -> Store:
+        return Store.build(operands=[vector, ref, indices])
+
+@irdl_op_definition
+class Fma(Operation):
+    name = "vector.fma"
+    vector1: Annotated[SSAValue, OperandDef(VectorType)]
+    vector2: Annotated[SSAValue, OperandDef(VectorType)]
+    vector3: Annotated[SSAValue, OperandDef(VectorType)]
+    res: Annotated[OpResult, ResultDef(VectorType)]
+
+    def verify_(self):
+        if self.vector1.typ.element_type != self.vector2.typ.element_type or self.vector2.typ.element_type != self.vector3.typ.element_type or self.vector3.typ.element_type != self.res.typ.element_type:
+            raise Exception(
+                "All vectors must have the same element type.")
+
+        if self.vector1.typ.get_num_dims() != self.vector2.typ.get_num_dims() or self.vector2.typ.get_num_dims() != self.vector3.typ.get_num_dims() or self.vector3.typ.get_num_dims() != self.res.typ.get_num_dims():
+            raise Exception("All vectors must have same number of dimensions.")
+
+    @staticmethod
+    def get(vector1: Operation | SSAValue, vector2: Operation | SSAValue,
+            vector3: Operation | SSAValue) -> Fma:
+        return Fma.build(operands=[vector1, vector2, vector3])
+
+Vector = Dialect([Load, Store, Fma], [])
